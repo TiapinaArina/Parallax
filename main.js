8b6e1b38225f85af0c8d81918a1eb73ac4bbea8d
Tiapina Arina <tiapinaarina@gmail.com>
2023-10-10 15:36:15 +0300

    Parallax end
    Добавила движение по Y

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('mousemove', function(e){
     document.querySelectorAll('.par').forEach(currentItem =>{
         let speed=currentItem.getAttribute('data-speed')
-        currentItem.style.transform=`translateX(${e.clientX*speed/1500}px)`
+        currentItem.style.transform=`translate(${e.clientX*speed/1500}px,${e.clientY*speed/1500}px)`
     })
 })
